82ac2458a6bd3947e1546cdad90418ada8ddcfe3
Pascal Ehlert <pascal@hacksrus.net>
2013-01-22 12:42:33 +0100

    Allow the execution of inline scripts again

--- vendor/assets/javascripts/jquery.pjax.js
+++ vendor/assets/javascripts/jquery.pjax.js
@@ -569,7 +569,7 @@ function extractContainer(data, xhr, options) {
     var $head = $(data.match(/<head[^>]*>([\s\S.]*)<\/head>/i)[0])
     var $body = $(data.match(/<body[^>]*>([\s\S.]*)<\/body>/i)[0])
   } else {
-    var $head = $body = $($.parseHTML(data))
+    var $head = $body = $($.parseHTML(data, document, true))
   }
 
   // If response data is empty, return fast
